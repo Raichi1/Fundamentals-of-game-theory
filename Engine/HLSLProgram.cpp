fcee185f495cc0a683dc1f0921d1ae0203965b0b
Renzo Andree Espiritu Cueva <u202113340@upc.edu.pe>
2023-04-27 19:46:57 -0500

    agregando proyecto Engine con dependencia

---
+++ Engine/HLSLProgram.cpp
@@ -0,0 +1,81 @@
+#include "HLSLProgram.h"
+#include "Error.h"
+#include <fstream>
+#include <vector>
+using namespace std;
+
+HLSLProgram::HLSLProgram():programID(0),vertexShaderID(0),fragmentShaderID(0), numAtribute(0) {
+}
+
+HLSLProgram::~HLSLProgram() {
+}
+
+void HLSLProgram::use() {
+}
+void HLSLProgram::unuse() {
+}
+void HLSLProgram::addAtribute(const string attributeName) {
+}
+void HLSLProgram::compileShaders(const string& vertexShaderFilePath, const string& fragmentShaderFilePath){
+	programID = glCreateProgram();
+	vertexShaderID = glCreateShader(GL_VERTEX_SHADER);
+	if (vertexShaderID == 0) {
+		fatalError("ERORR creating Vertex Shader");
+	}
+	fragmentShaderID = glCreateShader(GL_FRAGMENT_SHADER);
+	if (fragmentShaderID == 0) {
+		fatalError("ERROR creating Fragment Shader");
+	}
+	compileShader(vertexShaderFilePath, vertexShaderID);
+	compileShader(fragmentShaderFilePath, fragmentShaderID);
+}
+void HLSLProgram::compileShader(const string& shaderPath, GLuint id) {
+	string filecontent = "";
+	string line = "";
+	ifstream shaderFile(shaderPath);
+	if (shaderFile.fail()) {
+		fatalError("Coul not open " + shaderPath);
+	}
+	while (getline(shaderFile, line)) {
+		filecontent += line + "\n";
+	}
+	shaderFile.close();
+	const char* contents = filecontent.c_str();
+	glShaderSource(id, 1, &contents, nullptr);
+	glCompileShader(id);
+	GLint isCompiledd = 0;
+	glGetShaderiv(id, GL_COMPILE_STATUS, &isCompiledd);
+	if (isCompiledd == GL_FALSE) {
+		GLint maxLenght = 0;
+		glGetShaderiv(id, GL_INFO_LOG_LENGTH, &maxLenght);
+		vector<GLchar> infolog(maxLenght);
+		glGetShaderInfoLog(id, maxLenght, &maxLenght, &infolog[0]);
+		fatalError("Shader " + shaderPath + " could not compiled " + printf("%s", &(infolog[0])));
+		glDeleteShader(id);
+		return;
+	}
+}
+void HLSLProgram::linkShader() {
+	glAttachShader(programID, vertexShaderID);
+	glAttachShader(programID, fragmentShaderID);
+	glLinkProgram(programID);
+	GLint isLinked = 0;
+	glGetProgramiv(programID, GL_LINK_STATUS, (int*)&isLinked);
+	if (isLinked == GL_FALSE) {
+		GLint maxLength = 0;
+		glGetProgramiv(programID, GL_INFO_LOG_LENGTH, &maxLength);
+		vector<GLchar> infolog(maxLength);
+		glGetProgramInfoLog(programID, maxLength, &maxLength, &infolog[0]);
+		glDeleteProgram(programID);
+		fatalError("Shader couldn't link " + printf("%s", &(infolog[0])));
+		glDeleteShader(vertexShaderID);
+		glDetachShader(fragmentShaderID);
+		return;
+	}
+	glDetachShader(programID, vertexShaderID);
+	glDetachShader(programID, fragmentShaderID);
+	glDeleteShader(vertexShaderID);
+	glDetachShader(fragmentShaderID);
+
+
+}
---
+++ Engine/Sprite.cpp
@@ -0,0 +1,69 @@
+#include "Sprite.h"
+#include "Vertex.h"
+#include <cstddef>
+
+Sprite::Sprite() {
+	vboID = 0;
+}
+
+Sprite::~Sprite() {
+
+}
+void Sprite::init(float x, float y, int width, int height) {
+	this->x = x;
+	this->y = y;
+	this->width = width;
+	this->height = height;
+	if (vboID == 0) {
+		glGenBuffers(1, &vboID);
+	}
+	/*
+	float vertexData[12];
+	vertexData[0] = x + width;
+	vertexData[1] = y + height;
+
+	vertexData[2] = x;
+	vertexData[3] = y + height;
+
+	vertexData[4] = x;
+	vertexData[5] = y;
+
+	vertexData[6] = x;
+	vertexData[7] = y;
+
+	vertexData[8] = x + width;
+	vertexData[9] = y;
+
+	vertexData[10] = x + width;
+	vertexData[11] = y + height;*/
+
+	Vertex vertexData[6];
+	vertexData[0].setPosition(x + width, y + height);
+	vertexData[1].setPosition(x, y + height);
+	vertexData[2].setPosition(x, y);
+	vertexData[3].setPosition(x, y);
+	vertexData[4].setPosition(x + width, y);
+	vertexData[5].setPosition(x + width, y + height);
+	for (int i = 0; i < 6; i++) {
+		vertexData[i].setColor(1.0, 0, 0, 1.0);
+	}
+	//vertexData[2].setColor(0, 0, 1.0, 1.0);
+	//vertexData[4].setColor(0, 0.5, 0, 1.0);
+	glBindBuffer(GL_ARRAY_BUFFER, vboID);//Carga la información
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertexData), vertexData, GL_STATIC_DRAW);//Procesa la información
+	glBindBuffer(GL_ARRAY_BUFFER, 0);//Libera la información almacenada
+}
+void Sprite::draw() {
+	glBindBuffer(GL_ARRAY_BUFFER, vboID);//
+	//
+	//glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 0, 0);//Recibe 6 valores (inicio,tamaño(x,y), data type, normalizar)
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, position));
+
+
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 4, GL_UNSIGNED_BYTE, GL_FALSE, sizeof(Vertex), (void*)offsetof(Vertex, color));
+	glDrawArrays(GL_TRIANGLES,0,6);
+	glDisableVertexAttribArray(0);
+	glBindBuffer(GL_ARRAY_BUFFER, 0);//Limpia en memoria
+}
